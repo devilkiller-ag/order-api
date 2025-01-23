88c2db0930ebf2b179e42ee9077a35086b40ba66
devilkiller-ag <ashmitgupta.official@gmail.com>
2025-01-23 19:38:52 +0530

    chore: converted ApiError and Index into typescript

---
+++ src/utils/ApiError.ts
@@ -0,0 +1,27 @@
+/**
+ * Class for handling API errors.
+ */
+/**
+ * Class for handling API errors.
+ */
+class ApiError extends Error {
+  timestamp: string;
+  status: number;
+  error: string;
+  path: string;
+
+  constructor(
+    timestamp: string,
+    status: number,
+    error: string,
+    path: string
+  ) {
+    super();
+    this.timestamp = timestamp;
+    this.status = status;
+    this.error = error;
+    this.path = path;
+  }
+}
+
+export { ApiError };
