5e3eaf66d1eb7c5dba85b0b54fe13319049c77ba
devilkiller-ag <ashmitgupta.official@gmail.com>
2025-01-23 18:46:29 +0530

    setup: express application

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import dotenv from "dotenv";
+
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+app.get('/', (req, res) => {
+  res.send('Order API running successfully!');
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT ${PORT} at http://localhost:${PORT}`);
+});
